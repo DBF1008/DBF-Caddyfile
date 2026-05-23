18abf61155a012521a820a4eb7762fbcd73abb9b
dongbufan <dongbufan@qq.com>
2026-05-23 13:26:43 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	args := os.Args[1:]
 	if len(args) == 0 {
-		args = []string{"dev"}
+		args = []string{"dev", "-container-image", "docker.m.daocloud.io/library/postgres:15"}
 	}
 	os.Exit(cmd.Run(args))
 }
